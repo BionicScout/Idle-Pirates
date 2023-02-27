87bc741ed9d05096d9f0c559914d0714a804d5db
Jadervin <jdervin@ltu.edu>
2023-02-27 15:03:27 -0500

    Starting making scripts for spawning objects

--- Assets/Scripts/Mini Game Scripts/BackgroundMovement.cs
+++ Assets/Scripts/Mini Game Scripts/BackgroundMovement.cs
@@ -23,6 +23,39 @@ public class BackgroundMovement : MonoBehaviour
     [SerializeField]
     private float timer = 0;
 
+    [SerializeField]
+    private GameObject maxXPositionObj;
+
+    [SerializeField]
+    private GameObject minXPositionObj;
+
+    [SerializeField]
+    private GameObject maxYPositionObj;
+
+    [SerializeField]
+    private GameObject minYPositionObj;
+
+    [SerializeField]
+    private float maxXPosition;
+
+    [SerializeField]
+    private float minXPosition;
+
+    [SerializeField]
+    private float maxYPosition;
+
+    [SerializeField]
+    private float minYPosition;
+
+
+    [SerializeField]
+    private List<GameObject> obstaclePrefabList = new List<GameObject>();
+
+    [SerializeField]
+    private int maxSpawnItems = 3;
+
+    [SerializeField]
+    private int numOfSpawnItems;
 
 
     void Start()
@@ -35,14 +68,46 @@ public class BackgroundMovement : MonoBehaviour
 
         disappearTime = -(speed) * 3;
 
+
+        maxXPosition = maxXPositionObj.transform.position.x;
+        minXPosition = minXPositionObj.transform.position.x;
+        maxYPosition = maxYPositionObj.transform.position.y;
+        minYPosition = minYPositionObj.transform.position.y;
+
+
+        //when background is spawned, randomize obstacle position and 
+        //obstacles spawned
+
+        //Goes through each item in List and randomize position
+        for(int j = 0; j < obstaclePrefabList.Count;j++) 
+        {
+            obstaclePrefabList[j].transform.position = 
+                new Vector3(Random.Range(minXPosition, maxXPosition), 
+                Random.Range(minYPosition, maxYPosition), 0);
         
+        }
+
+
+        //randomize number of objects that will spawn
+        numOfSpawnItems = Random.Range(1, maxSpawnItems);
+
+
+        //spawn random objects up to the number of items that can be spawned
+        for(int i = 0; i < numOfSpawnItems; i++) 
+        {
+            int objectSpawnNumber = Random.Range(0, obstaclePrefabList.Count - 1);
+
+            Instantiate(obstaclePrefabList[objectSpawnNumber], 
+                transform.position, transform.rotation);
+
+        }
+
+
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
-
+    { 
 
         if (timer < disappearTime)
         {
