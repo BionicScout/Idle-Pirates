7cd4151e8e8bbaf81f798de67b4552fb0f216773
Jadervin <jdervin@ltu.edu>
2023-04-22 00:33:15 -0400

    Added new background and auto saving

--- Assets/Scripts/Saving/GameMaster.cs
+++ Assets/Scripts/Saving/GameMaster.cs
@@ -2,8 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameMaster : MonoBehaviour {
+public class GameMaster : MonoBehaviour 
+{
     GameData saveData = new GameData();
+
+
+    private void Start()
+    {
+        if(SceneSwitcher.instance.GetCurrentScene() == "Map Scene")
+        {
+            Debug.Log("Saved from Menu");
+            SaveStateManager.instance.SaveGame(saveData);
+        }
+    }
 
     public void OnSaveButtonClick() {
         SaveStateManager.instance.SaveGame(saveData);
